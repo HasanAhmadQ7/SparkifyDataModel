80aadb35a6cd5cc35cbf4af5f396443f48a579e0
HasanAhmadQ7 <hasanahmad@Hasans-MacBook.local>
2020-02-04 17:36:04 +0900

    code formatted using black and flake8

--- etl.py
+++ etl.py
@@ -11,13 +11,28 @@ def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath, lines=True)
     # insert song record
-    song_data = list(df.loc[0, ['song_id', 'title', 'artist_id', 'year', 'duration']].values)
+    song_data = list(
+        df.loc[0, ["song_id", "title", "artist_id", "year", "duration"]].values
+    )
     cur.execute(song_table_insert, song_data)
 
     # insert artist record
     artist_data = list(
-        df.loc[0, ['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values)
-    artist_data = [None if isinstance(el, np.float64) and np.isnan(el) else el for el in artist_data]
+        df.loc[
+            0,
+            [
+                "artist_id",
+                "artist_name",
+                "artist_location",
+                "artist_latitude",
+                "artist_longitude",
+            ],
+        ].values
+    )
+    artist_data = [
+        None if isinstance(el, np.float64) and np.isnan(el) else el
+        for el in artist_data
+    ]
     cur.execute(artist_table_insert, artist_data)
 
 
@@ -26,14 +41,16 @@ def process_log_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df[df['page'] == "NextSong"]
+    df = df[df["page"] == "NextSong"]
 
     # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'])
+    t = pd.to_datetime(df["ts"])
 
     # insert time data records
-    time_data = (t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday)
-    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_data = (t, t.dt.hour, t.dt.day, t.dt.week,
+                 t.dt.month, t.dt.year, t.dt.weekday)
+    column_labels = ("start_time", "hour", "day",
+                     "week", "month", "year", "weekday")
     combined_dict = dict(zip(column_labels, time_data))
     time_df = pd.DataFrame(combined_dict)
 
@@ -41,7 +58,7 @@ def process_log_file(cur, filepath):
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -60,7 +77,16 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
         # insert songplay record
         songplay_data = (
-        index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+            index,
+            row.ts,
+            row.userId,
+            row.level,
+            songid,
+            artistid,
+            row.sessionId,
+            row.location,
+            row.userAgent,
+        )
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -68,27 +94,29 @@ def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, '*.json'))
+        files = glob.glob(os.path.join(root, "*.json"))
         for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+        print("{}/{} files processed.".format(i, num_files))
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student"
+    )
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     conn.close()
 
